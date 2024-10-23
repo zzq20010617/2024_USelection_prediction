33da8578d79d094d4f52669e6a07b942260a68cb
Ziqi Zhu <zzq20010617@gmail.com>
2024-10-23 10:05:39 -0500

    remove some code from starter folder, add references

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -15,7 +15,7 @@ library(tidyverse)
 #### Read data ####
 poll_data <- read_csv("data/02-analysis_data/cleaned_data.csv")
 candidate_avg_pct <- poll_data %>%
-  group_by(candidate_name) %>%
+  group_by(answer) %>%
   summarize(average_pct = mean(pct, na.rm = TRUE))
 
 # View the result
@@ -23,19 +23,6 @@ print(candidate_avg_pct, n=60)
 top_10_candidates <- candidate_avg_pct %>%
   arrange(desc(average_pct)) %>%  # Sort by average_pct in descending order
   head(10)
-
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
-
 
 #### Save model ####
 saveRDS(
